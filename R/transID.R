82c6da448e43871601b164d157a73819e66b7a65
Yunze Liu <jieandze1314@gmail.com>
2023-03-01 08:00:02 +0000

    version 1.1.3

--- R/transID.R
+++ R/transID.R
@@ -38,9 +38,7 @@ transId <- function(id,
   #--- args ---#
   org <- mapEnsOrg(organism = tolower(org))
   # if id has ensembl version, remove them
-  if (any(grepl("ENS[A-Z][0-9]{4,}", id))) {
-    id <- stringr::str_split(id, "\\.", simplify = T)[, 1]
-  }
+  # if(all(all %>% stringr::str_detect(.,'ENS'))) id <- stringr::str_split(id, "\\.", simplify = T)[, 1]
 
   transTo <- sapply(tolower(transTo), function(x) {
     if (grepl(x, "entrezid")) x <- "entrezid"
